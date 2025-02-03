ec1a03edf98d285069d592a86a84830fa1084840
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2025-02-03 11:44:24 +0530

    achieving functionality locally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { AddTodoComponent } from "../shared/add-todo/add-todo.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddTodoComponent],
+  imports: [ AddTodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/shared/add-todo/add-todo.component.ts
+++ src/shared/add-todo/add-todo.component.ts
@@ -1,19 +1,54 @@
+
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import { ViewTodoComponent } from "./view-todo/view-todo.component";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'; 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
 
 
 @Component({
   selector: 'app-add-todo',
-  imports: [MatIconModule, MatDividerModule, MatButtonModule, MatInputModule, FormsModule, MatFormFieldModule, ViewTodoComponent],
+  imports:[MatFormFieldModule,MatInputModule,MatIconModule,FormsModule,CommonModule],
   templateUrl: './add-todo.component.html',
   styleUrl: './add-todo.component.css'
 })
 export class AddTodoComponent {
+  task: string = '';
+  description: string = '';
+  tasks: { task: string, description: string }[] = [];
+  editIndex: number | null = null; 
 
+
+
+  addTask() {
+    if (this.task.trim() && this.description.trim()) {
+      if (this.editIndex === null) {
+        this.tasks.push({ task: this.task, description: this.description });
+      } else {
+        this.tasks[this.editIndex] = { task: this.task, description: this.description };
+        this.editIndex = null; 
+      }
+      console.log(this.tasks);
+      this.task = '';
+      this.description = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+    if (this.editIndex === index) {
+      this.task = '';
+      this.description = '';
+      this.editIndex = null;
+    }
+  }
+
+  editTask(index: number) {
+    this.task = this.tasks[index].task;
+    this.description = this.tasks[index].description;
+    this.editIndex = index;
+  }
 }
+
